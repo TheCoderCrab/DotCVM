b92c0208538c70662587215f384a5e6bafe56ffc
TheCoderCrab <54598040+TheCoderCrab@users.noreply.github.com>
2020-09-06 21:14:57 +0100

    Changed release workdir * 2

--- bscripts/install_modules.sh
+++ bscripts/install_modules.sh
@@ -3,6 +3,6 @@
 
 echo -e "\e[1m\e[32mInstalling dotcvm standard modules\e[0m\e[39m"
 
-mkdir -p $HOME/.dotcvm/modules
-cp -r modules/* $HOME/.dotcvm/modules
+mkdir -p $HOME/.local/share/dotcvm/modules
+cp -r modules/* $HOME/.local/share/dotcvm/modules
 echo -e "\e[1m\e[32mDotCVM standard modules installed\e[0m\e[39m"
