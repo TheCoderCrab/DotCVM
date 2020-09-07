b452a3af311509b8cb538b3b02fe9364a06444ca
TheCoderCrab <54598040+TheCoderCrab@users.noreply.github.com>
2020-09-07 17:17:03 +0100

    Changed uninstall script

--- bscripts/uninstall.sh
+++ bscripts/uninstall.sh
@@ -14,7 +14,7 @@ echo -e "\e[1m\e[32mUninstalling dotcvm\e[0m\e[39m"
 if [ ${EUID:-$(id -u)} -eq 0 ]; then # Should be run as root
     [ -f /usr/share/applications/dotcvm.desktop ] && rm /usr/share/applications/dotcvm.desktop
     [ -f /usr/bin/dotcvm ] && rm /usr/bin/dotcvm
-    [ -d $HOME/.dotcvm ] && rm -r "$HOME/.dotcvm"
+    [ -d $HOME/.dotcvm ] && rm -r "$HOME/.local/share/dotcvm/"
     echo -e "\e[1m\e[32mDone\e[0m\e[39m"
     [ -d "$DC_INSTALL_DIR" ] && rm -r "$DC_INSTALL_DIR" # This is here because the uninstall 
                                                         # script is probably inside the install
